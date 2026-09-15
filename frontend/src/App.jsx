01c8524ddcab776928e18820cb50dc9a8a65070f
Daoli <dli245@aucklanduni.ac.nz>
2026-09-15 15:40:33 +1200

    Add Navbar to app layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllArticles from "./pages/AllArticles";
-import SearchArticles from "./pages/SearchArticles";
 import ArticleDetail from "./pages/ArticleDetail";
 import CreateArticle from "./pages/CreateArticle";
 import EditArticle from "./pages/EditArticle";
 import Login from "./pages/Login";
 import Notifications from "./pages/Notifications";
 import Profile from "./pages/Profile";
+import SearchArticles from "./pages/SearchArticles";
+import Navbar from "./components/Navbar";
 
 function App() {
 
     return (
         <BrowserRouter>
+
+            <Navbar />
+
             <Routes>
                 <Route path="/articles" element={<AllArticles />} />
-                <Route path="/search" element={<SearchArticles />} />
-                <Route path="/articles/create" element={<CreateArticle />} />
                 <Route path="/articles/:aid" element={<ArticleDetail />} />
                 <Route path="/articles/:aid/edit" element={<EditArticle />} />
+                <Route path="/articles/create" element={<CreateArticle />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/notifications" element={<Notifications />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/search" element={<SearchArticles />} />
             </Routes>
+
         </BrowserRouter>
     );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+    return (
+        <nav>
+            <Link to="/articles">
+                Articles
+            </Link>
+
+            <Link to="/search">
+                Search
+            </Link>
+
+            <Link to="/articles/create">
+                Create Article
+            </Link>
+
+            <Link to="/profile">
+                My Profile
+            </Link>
+
+            <Link to="/notifications">
+                Notifications
+            </Link>
+        </nav>
+    );
+}
+
+export default Navbar;
