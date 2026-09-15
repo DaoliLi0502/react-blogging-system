d6f373813db435818f75fa0013d83062aa04f156
Daoli <dli245@aucklanduni.ac.nz>
2026-09-15 16:41:42 +1200

    Improve navbar authentication handling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,66 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Navbar() {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+
+        const fetchCurrentUser = async () => {
+
+            try {
+
+                const response = await axios.get(
+                    "http://localhost:3000/api/users/me",
+                    {
+                        withCredentials: true
+                    }
+                );
+
+                setUser(response.data.user);
+
+            } catch (error) {
+
+                setUser(null);
+            }
+        };
+
+        fetchCurrentUser();
+
+    }, []);
+
+    if (!user) {
+
+        return (
+            <nav>
+                <Link to="/articles">
+                    Articles
+                </Link>
+
+                <Link to="/search">
+                    Search
+                </Link>
+
+                <Link to="/login">
+                    Create Article
+                </Link>
+
+                <Link to="/login">
+                    Notifications
+                </Link>
+
+                <Link to="/login">
+                    My Profile
+                </Link>
+
+                <Link to="/login">
+                    Login
+                </Link>
+            </nav>
+        );
+    }
 
     return (
         <nav>
@@ -10,10 +70,6 @@ function Navbar() {
 
             <Link to="/articles/create">
                 Create Article
-            </Link>
-
-            <Link to="/login">
-                Login
             </Link>
 
             <Link to="/notifications">
